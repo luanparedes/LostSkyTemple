5d0d5a1a0b06f57bffe80a51f23d8960ea5bdfe0
Luan Paredes <luan.developer.ti@gmail.com>
2023-10-27 18:06:30 -0300

    LST_0044: Adding C++ class and game was builded for the first time.

---
+++ Source/LostSkyTemple/LostSkyTemple.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LostSkyTemple.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LostSkyTemple, "LostSkyTemple" );
