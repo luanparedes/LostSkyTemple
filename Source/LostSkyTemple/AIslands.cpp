5a3a7fb8bf1b9f5ba077f838d558fda2843e08f0
Luan Paredes <luan.developer.ti@gmail.com>
2023-11-02 23:33:03 -0300

    LST_0053: Getting actor location

--- Source/LostSkyTemple/AIslands.cpp
+++ Source/LostSkyTemple/AIslands.cpp
@@ -12,7 +12,7 @@ void AAIslands::BeginPlay()
 {
 	Super::BeginPlay();
 
-	getInitialPositionValues();
+	GetInitialPositionValues();
 }
 
 // Called every frame
@@ -20,55 +20,14 @@ void AAIslands::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	if(IslandPosition.X >= maxMoveX)
-		isReverseX = true;
-	else if(IslandPosition.X <= minMoveX)
-		isReverseX = false;
-
-	if(IslandPosition.Y >= maxMoveY)
-		isReverseY = true;
-	else if(IslandPosition.Y <= minMoveY)
-		isReverseY = false;
-
-	if(IslandPosition.Z >= maxMoveZ)
-		isReverseZ = true;
-	else if(IslandPosition.Z <= minMoveZ)
-		isReverseZ = false; 
-
-	
-	if(!isReverseX)
-		IslandPosition.X += XYZspeed.X;
-	else
-		IslandPosition.X -= XYZspeed.X;
-
-	if(!isReverseY)
-		IslandPosition.Y += XYZspeed.Y;
-	else
-		IslandPosition.Y -= XYZspeed.Y;
-
-	if(!isReverseZ)
-		IslandPosition.Z += XYZspeed.Z;
-	else
-		IslandPosition.Z -= XYZspeed.Z;
+	IslandPosition = GetActorLocation();
+	IslandPosition.X += XYZspeed.X;
+	IslandPosition.Y += XYZspeed.Y;
+	IslandPosition.Z += XYZspeed.Z;
 
 	SetActorLocation(IslandPosition);
-
-	// Island moves forward
-		// Get island location
-		// Move using a limit
-	
-	// Island moves backward
-		// Get Island location
-		// Move using a limit
 }
 
-void AAIslands::getInitialPositionValues(){
-	maxMoveX += IslandPosition.X;
-	minMoveX = IslandPosition.X - minMoveX;
-
-	maxMoveY += IslandPosition.Y;
-	minMoveY = IslandPosition.Y - minMoveY;
-
-	maxMoveZ += IslandPosition.Z;
-	minMoveZ = IslandPosition.Z - minMoveZ;
+void AAIslands::GetInitialPositionValues(){
+	IslandPosition = GetActorLocation();
 }
