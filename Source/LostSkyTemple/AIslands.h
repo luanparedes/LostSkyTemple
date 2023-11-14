a556043837ce621662c69d4692353fad0c4f3062
Luan Paredes <luan.developer.ti@gmail.com>
2023-11-14 00:19:48 -0300

    LST_0063: Methods Move and Rotate created - Islands rotation implemented

--- Source/LostSkyTemple/AIslands.h
+++ Source/LostSkyTemple/AIslands.h
@@ -23,10 +23,18 @@ public:
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	float MoveDistance = 100;
 
+	UPROPERTY(EditAnywhere, Category="Rotation Platform")
+	FRotator IslandRotation = FRotator();
+
+	UPROPERTY(EditAnywhere, Category="Rotation Platform")
+	FRotator XYZRotationSpeed = FRotator();
+
 protected:
 	virtual void BeginPlay() override;
 
 private:
 	FVector StartLocation;
 	void GetInitialPositionValues();
+	void MoveIsland(float DeltaTime);
+	void RotateIsland(float DeltaTime);
 };
